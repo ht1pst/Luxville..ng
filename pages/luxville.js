32187222745b4065f58dc4e327d7ac6400e21961
ht1pst <funtoobadua@gmail.com>
2024-11-29 12:18:46 +0100

    updated code

--- pages/luxville.js
+++ pages/luxville.js
@@ -3,3 +3,11 @@ $(function(){
     $(".section2").hide().fadeIn(8000)
 });
 
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
